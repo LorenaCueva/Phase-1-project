9caa1669c0492459e45c846021bd37e99abc8ffd
Lorena Cueva <lorenacueva@gmail.com>
2022-05-03 15:59:02 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Phase 1 Project - Brewery Tracker ??
+
+## smaller header
+
+1.
+2.
+3.
+
+``````
+Code format
+```````
+
+Overview
+
+Features
+
+MVP
---
+++ javascripts/index.js
@@ -0,0 +1,36 @@
+/* Global Variables */
+
+
+
+
+
+/* Node Getters */
+
+const mainDiv = () => document.getElementById('main');
+
+
+
+/* Event Listeners */
+
+
+
+
+/* Event Handlers */
+const loadHome = () => {
+    resetMainDiv();
+    //add to it
+}
+
+
+
+/* MISC */
+const resetMainDiv = () => {
+    mainDiv().innerHTML = '';
+}
+
+
+
+
+/* Startup */
+
+// document.addEventListener('DOMContentLoader', function(){} )
